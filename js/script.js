30560b8ed48f5d603b2b34c306537d32ad52dcde
Anurella <ndukwuamaka@yahoo.com>
2017-01-21 11:25:37 +0000

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-$(function() {
-  //@ find a href that starts with a # but has other chracters after it
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          // @2000 is the time the scroll takes to get to the div 
-          scrollTop: target.offset().top
-        }, 2000);
-        return false;
-      }
-    }
-  });
+$(document).ready(function() {
+	
+	$("a.transition").click(function(event){
+		event.preventDefault();
+		linkLocation = this.href;
+		$("body").fadeOut(3000, redirectPage);		
+	});
+		
+	function redirectPage() {
+		window.location = linkLocation;
+	}
+	
 });
+
+
+
+
+
+
